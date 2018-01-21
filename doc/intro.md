f75d1ddbc8eaa3f5b19a467df1a92222b46c7d90
Ben Kovitz <bkovitz@indiana.edu>
2018-01-20 21:05:33 -0500

    Initial commit.
    Pulled out of my old FARGish util directories.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to state-threading
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject with-state "0.0.1-SNAPSHOT"
+  :description
+    "The with-state macro: when '->' isn't good enough."
+  :url "https://github.com/bkovitz/state-threading"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [farg/pmatch "0.1.0-SNAPSHOT"]])
