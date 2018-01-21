2cce238038a36fd8cd98806af07fba36656c1ed6
Ben Kovitz <bkovitz@indiana.edu>
2018-01-20 21:16:38 -0500

    Corrected URL

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject farg/with-state "0.0.1-SNAPSHOT"
   :description
     "The with-state macro: when '->' isn't good enough."
-  :url "https://github.com/bkovitz/state-threading"
+  :url "https://github.com/bkovitz/with-state"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
